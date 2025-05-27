16e2144807b3fd56bb82c967eacbd064bc031252
NoemieBenard <noemie.benard@gmail.com>
2025-05-27 15:05:25 +0200

    commit before pull

--- Assets/Script/HUDController.cs
+++ Assets/Script/HUDController.cs
@@ -16,8 +16,8 @@ public class HUDController : MonoBehaviour
     [SerializeField] private string talkMessage = "Parler (F)";
     
     [Header("Position")]
-    [SerializeField] private float pickupOffset = 100f;
-    [SerializeField] private float npcOffset = 220f; // Offset plus élevé pour les PNJ
+    [SerializeField] private float pickupOffset = 1.0f; // 1 unité monde au-dessus de l'objet
+    [SerializeField] private float npcOffset = 2.0f; // 2 unités monde au-dessus du NPC
 
     private void Awake()
     {
@@ -38,13 +38,17 @@ public class HUDController : MonoBehaviour
 
         // on change le texte en fonction de l'interaction
         interactionText.text = isNPC ? talkMessage : pickupMessage;
-
-        // Convertir la position monde en position écran
-        Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
         
-        // Utiliser un offset différent selon le type d'objet
-        screenPosition.y += isNPC ? npcOffset : pickupOffset;
-        
-        interactionUI.transform.position = screenPosition;
+        // Configurer le RectTransform pour un positionnement correct
+        RectTransform rectTransform = interactionUI.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // Positionner directement en coordonnées monde avec l'offset approprié
+            rectTransform.position = new Vector3(
+                worldPosition.x,
+                worldPosition.y + (isNPC ? npcOffset : pickupOffset), // Utiliser l'offset approprié
+                worldPosition.z
+            );
+        }
     }
 }
